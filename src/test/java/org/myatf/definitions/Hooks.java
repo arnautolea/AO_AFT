ada23556113ce0074be42a92e307b672720caa07
Arnaut Olga <arnaut.olea@mail.ru>
2023-11-06 18:34:23 +0200

    change of PageFactory from By to WebElement usage split UI stepDefinitions by feature files add of BasePage Context update, call for Keys instead of Strings

--- src/test/java/org/myatf/definitions/Hooks.java
+++ src/test/java/org/myatf/definitions/Hooks.java
@@ -7,8 +7,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.myatf.utils.ScenarioContext;
 import org.myatf.utils.WebDriverFactory;
-import org.openqa.selenium.WebDriver;
-
 
 public class Hooks {
     private static final Logger logger = LogManager.getLogger(Hooks.class);
